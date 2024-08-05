481f95bafdde49e5a69c07f6aa25a1fc8c2e432e
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-08-05 19:24:46 +0500

    added Award Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { Badge, Banner, CustomFlowbiteTheme } from "flowbite-react";
+import { Banner } from "flowbite-react";
 import { HiX } from 'react-icons/hi';
 import { MdAnnouncement } from "react-icons/md";
+import Awards from "./components/Awards";
 
 export default function Home() {
     return (
@@ -84,38 +85,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-
-            <div className="">
-                <h1 className="mb-6 text-xl font-semibold text-green-700">Awards</h1>
-                <div className="border border-gray-300 p-3 rounded-xl mb-8">
-                    <div className="mb-6 flex items-center gap-8">
-                        <div className="w-24 h-24 flex-shrink-0">
-                            <img className="w-full h-full object-cover rounded-full" src="img/resume/hackathon/gds_logo.webp" alt="Astana IT University Logo" />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <h3 className="font-medium">Winner of Google Developer Hackathon organized by GDSC AITU</h3>
-                            <div className="text-gray-700"><span className="font-semibold">Task:</span> create a chatbot that generates personalized routes on map to Astana's attractions based on user location, preferences, attraction hours, and travel mode. It also provides information on attractions, including descriptions, significance, hours, ticket prices, addresses, transport routes, and contact details.</div>
-                            <div className="text-gray-700"><span className="font-semibold">Prize:</span> 250 000₸ ( ≈ 500$ )</div>
-                        </div>
-                    </div>
-                    <div className="flex justify-between">
-                        <div className="flex gap-2">
-                            <Badge color="info">Articial Intelligance</Badge>
-                            <Badge color="info">Web Application</Badge>
-                        </div>
-                        <div className="flex items-center justify-end gap-2">
-                            <a className="flex items-center gap-1" href="certificates/gds_certificate.jpg" target="_blank">
-                                [<span className="text-sky-800 hover:text-gray-500">Repository</span>]
-                            </a>
-                            <a className="flex items-center gap-1" href="certificates/gds_certificate.jpg" target="_blank">
-                                [<span className="text-sky-800 hover:text-gray-500">Certificate</span>]
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
+            <Awards />
         </main>
     );
 }
